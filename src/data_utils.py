80ed7fdeadbbd401ca5ea7aa7c955ee7b3de2d7f
Sherrie Wang <sherwang@stanford.edu>
2018-03-08 13:45:11 -0800

    added training notebook

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,1 +1,11 @@
-Someone
+import numpy as np
+
+def clip_and_scale_image(img, img_type='naip', clip_min=0, clip_max=10000):
+    """
+    Clips and scales bands to between [0, 1] for NAIP, RGB, and Landsat
+    satellite images. Clipping applies for Landsat only.
+    """
+    if img_type in ['naip', 'rgb']:
+        return img / 255
+    elif img_type == 'landsat':
+        return np.clip(img, clip_min, clip_max) / (clip_max - clip_min)
